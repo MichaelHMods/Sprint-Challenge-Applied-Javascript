1929677ced16594ab9bb444903b818def501d1a5
MichaelHMods <mikeguadalupe@gmail.com>
2019-08-09 11:54:51 -0500

    header and tabs not showing up.

--- components/Header/index.js
+++ components/Header/index.js
@@ -9,4 +9,24 @@
 //  </div >
 // And add it to the DOM in the .headerContainer component
 
-function Header() {}
+function Header(obj) {
+    const theHeader = document.createElement('header');
+    const dateSpan = document.createElement('span');
+    const headerH1 = document.createElement('h1');
+    const tempSpan = document.createElement('span');
+
+    theHeader.classList.add('header');
+    dateSpan.classList.add('date');
+    tempSpan.classList.add('temp');
+
+    headContainer = document.querySelector('.header-container');
+    console.log(headContainer)
+    
+    headContainer.appendChild(theHeader);
+    theHeader.appendChild(dateSpan);
+    theHeader.appendChild(headerH1);
+    theHeader.appendChild(tempSpan);
+    
+}
+
+
--- components/Tabs/index.js
+++ components/Tabs/index.js
@@ -7,3 +7,40 @@
 //
 //  The tab component should look like this:
 //    <div class="tab">topic here</div>
+
+
+function theTabs(obj){
+axios.get('https://lambda-times-backend.herokuapp.com/topics')
+.then((response) => {
+    console.log(response);
+})
+//elements to iterate "javascript", "bootstrap", "technology", "jquery", "node.js"
+const java = document.createElement('div');
+java.classList.add('tab');
+java.textContent = `javascript`;
+
+const boot = document.createElement('div');
+boot.classList.add('tab');
+boot.textContent = `bootstrap`;
+
+const tech = document.createElement('div');
+tech.classList.add('tab');
+tech.textContent = `technology`;
+
+const jquer = document.createElement('div');
+jquer.classList.add('tab');
+jquer.textContent = `jquery`;
+
+const jsNode = document.createElement('div');
+jsNode.classList.add('tab');
+jsNode.textContent = `node.js`;
+
+
+return 
+
+}
+
+
+
+
+
