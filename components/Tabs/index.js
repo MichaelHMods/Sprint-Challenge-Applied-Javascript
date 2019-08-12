060f74d004a1bdb00fd4d52825120ade4417f6ad
MichaelHMods <mikeguadalupe@gmail.com>
2019-08-12 09:14:13 -0500

    corrected Header and tabs

--- components/Tabs/index.js
+++ components/Tabs/index.js
@@ -7,48 +7,67 @@
 //
 //  The tab component should look like this:
 //    <div class="tab">topic here</div>
+// let a = '';
 
 
-function theTabs(obj){
-axios.get('https://lambda-times-backend.herokuapp.com/topics')
-.then((response) => {
-    console.log(response);
-})
-//elements to iterate "javascript", "bootstrap", "technology", "jquery", "node.js"
-const java = document.createElement('div');
-java.classList.add('tab');
-java.textContent = `javascript`;
+// console.log(a);
 
-const boot = document.createElement('div');
-boot.classList.add('tab');
-boot.textContent = `bootstrap`;
+// function theTabs(obj){
+// //elements to iterate "javascript", "bootstrap", "technology", "jquery", "node.js"
+// const java = document.createElement('div');
+// java.classList.add('tab');
+// java.textContent = `javascript`;
 
-const tech = document.createElement('div');
-tech.classList.add('tab');
-tech.textContent = `technology`;
+// const boot = document.createElement('div');
+// boot.classList.add('tab');
+// boot.textContent = `bootstrap`;
 
-const jquer = document.createElement('div');
-jquer.classList.add('tab');
-jquer.textContent = `jquery`;
+// const tech = document.createElement('div');
+// tech.classList.add('tab');
+// tech.textContent = `technology`;
 
-const jsNode = document.createElement('div');
-jsNode.classList.add('tab');
-jsNode.textContent = `node.js`;
+// const jquer = document.createElement('div');
+// jquer.classList.add('tab');
+// jquer.textContent = `jquery`;
 
-const tabSection = document.querySelector('.topics')
+// const jsNode = document.createElement('div');
+// jsNode.classList.add('tab');
+// jsNode.textContent = `node.js`;
 
+// const tabSection = document.querySelector('.topics')
 
-console.log('testing');
-// nest section
-tabSection.appendChild(java);
-tabSection.appendChild(BOOT);
-tabSection.appendChild(tech);
-tabSection.appendChild(jquer);
-tabSection.appendChild(jsNode);
+// // nest section
+// tabSection.appendChild(java);
+// tabSection.appendChild(boot);
+// tabSection.appendChild(tech);
+// tabSection.appendChild(jquer);
+// tabSection.appendChild(jsNode);
 
-console.log(tabSection);
-return tabSection;
+// console.log(tabSection);
+// return tabSection;
+
+// }
+
+function theTabs(tabTitle){
+    let tab = document.createElement('div');
+    tab.classList.add('tab');
+    tab.textContent = tabTitle;
+
+    return tab;
+
 }
+const topics = document.querySelector('.topics');
+
+axios.get('https://lambda-times-backend.herokuapp.com/topics')
+.then(response => {
+    console.log(response.data);
+    response.data.topics.forEach(topic => {
+        topics.appendChild(theTabs(topic));
+
+    }) 
+})
+.catch((error) => {console.log(error)});
+
 
 
 
